ba7d89cb8343f2f59fc4d5d550b7e7b3ddeb10bb
Alex Webber <webbeale000@usd232.org>
2016-11-11 22:40:02 -0600

    Base Hello World

--- src/main/java/org/usd232/hackathon/twentysixteen/Main.java
+++ src/main/java/org/usd232/hackathon/twentysixteen/Main.java
@@ -1,7 +1,9 @@
 package org.usd232.hackathon.twentysixteen;
 
+import static spark.Spark.*;
+
 public class Main {
 	public static void main(String[] args) {
-
+		get("/hello", (req, res) -> "Hello World");
 	}
 }
