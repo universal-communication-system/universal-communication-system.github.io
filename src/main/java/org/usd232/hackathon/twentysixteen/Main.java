0a9213e28bee328142bb9fabb05b0e8d4de804b3
Zach Deibert <zachdeibert@gmail.com>
2016-11-12 01:02:45 -0600

    Removed files from other repo

--- src/main/java/org/usd232/hackathon/twentysixteen/Main.java
+++
@@ -1,9 +0,0 @@
-package org.usd232.hackathon.twentysixteen;
-
-import static spark.Spark.*;
-
-public class Main {
-	public static void main(String[] args) {
-		get("/hello", (req, res) -> "Hello World");
-	}
-}
